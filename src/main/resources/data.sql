446487eec9f9bb48eaf305cb8b01d49b26b7d203
HaolunLiu <77357970+HaolunLiu@users.noreply.github.com>
2024-02-21 21:34:18 -0800

    Delete data.sql

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO student (name, weight, height, hair_color, gpa) VALUES ('Bobby', 150, 30, 'red', 2.5);
-INSERT INTO student (name, weight, height, hair_color, gpa) VALUES ('Steve', 200, 30, 'blue', 3.6);
-INSERT INTO student (name, weight, height, hair_color, gpa) VALUES ('Dan', 120, 50, 'green', 4.0);
